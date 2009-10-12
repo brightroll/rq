5bcb3bcf246fbdf62e61c84351bcb2c3f0b3d677
Dru Nelson <drudru@gmail.com>
2009-10-11 22:42:17 -0700

    Added comments to rack router and cleaned whitespace

--- code/server.rb
+++ code/server.rb
@@ -8,9 +8,9 @@ gem_paths = [File.expand_path(File.join("code", "vendor", "gems")),  Gem.default
 Gem.clear_paths
 Gem.send :set_paths, gem_paths.join(":")
 
- 
+
 require 'rack'
- 
+
 # fastcgi_log = File.open("fastcgi.log", "a")
 # STDOUT.reopen fastcgi_log
 # STDERR.reopen fastcgi_log
@@ -32,19 +32,31 @@ class MiniRouter
   def call(env)
     path = env["PATH_INFO"].to_s.squeeze("/")
     p "PATH: #{path}"
+
+    ## Notice the chaining here, if a path below has a dependency,
+    ## that dependency must be handled prior, otherwhise an infinite
+    ## redirect loop will occur
+
+    # Gotta deal with static stuff first
     if path.index('/css') or path.index('/javascripts') or path.index('/favicon.ico')
       load 'code/main.rb'
       return Rack::Static.new(nil, :urls => ["/css", "/javascripts", "/favicon.ico"], :root => 'code/public').call(env)
     end
+
+    # Is this an install?
     if path == '/install'
       load 'code/install.rb'
       return RQ::Install.new.call(env)
     end
+
+    # Not set up?? It is an install
     if not File.exists?('config')
       resp = Rack::Response.new()
       resp.redirect('/install')
       return resp.finish
     end
+
+    # Everything else goes into main
     if true #path == '/'
       load 'code/main.rb'
       #return Rack::Static.new(RQ::Main.new, :urls => ["/css", "/javascripts"], :root => 'code/public').call(env)
@@ -54,7 +66,7 @@ class MiniRouter
 end
 
 
-     
+
 #Dir.chdir('code')
 
 #use Rack::ShowExceptions
