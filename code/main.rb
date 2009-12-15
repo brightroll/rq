2120a1d489f211332f694a09f21f5d97f1f80922
Dru Nelson <drudru@gmail.com>
2009-12-14 16:43:10 -0800

    Worked on http api and tested message submission

--- code/main.rb
+++ code/main.rb
@@ -61,10 +61,17 @@ module RQ
       # check for queue
       this_queue = "http://#{request.host}:#{request.port}/q/#{params[:name]}"
 
-      if this_queue == params['mesg']['dest']
+      api_call = params.fetch('x_format', 'html')
+      if api_call == 'json'
+        prms = JSON.parse(params['mesg'])
+      else
+        prms = params['mesg']
+      end
+
+      if this_queue == prms['dest']
         q_name = params[:name]
       else
-        if params['mesg']['relay_ok'] == 'yes'
+        if prms['relay_ok'] == 'yes'
           q_name = 'relay' # Relay is the special Q
         else
           throw :halt, [404, "404 - Not this Queue. Relaying not allowed"]
@@ -77,20 +84,46 @@ module RQ
         throw :halt, [404, "404 - Queue not found"]
       end
 
-      result = qc.create_message(params['mesg'])
-      "We got <pre> #{params.inspect} </pre> from form, and #{result} from Queue #{q_name}"
+      result = qc.create_message(prms)
+
+      if api_call == 'json'
+        "#{result.to_json}"
+      else
+        "We got <pre> #{prms.inspect} </pre> from form, and #{result} from Queue #{q_name}"
+      end
     end
 
     get '/q/:name/:msg_id' do
       # check for queue
       # TODO: sanitize names (no dots or slashes)
+
+      fmt = :html
+      msg_id = params['msg_id']
+
+      extension = msg_id[-5..-1]
+      if extension == '.json'
+        fmt = :json
+        msg_id = msg_id[0..-6]
+      end
+
       qc = RQ::QueueClient.new(params[:name])
 
       if not qc.exists?
         throw :halt, [404, "404 - Queue not found"]
       end
 
-      erb :message
+      ok, msg = qc.get_message({ 'msg_id' => msg_id })
+
+      if ok != 'ok'
+        throw :halt, [404, "404 - Message ID not found"]
+      end
+
+      if fmt == :html
+        erb :message, { :locals => { 'msg_id' => msg_id, 'msg' => msg } }
+      else
+        #content_type 'application/json'
+        msg.to_json
+      end
     end
 
     post '/q/:name/:msg_id' do
