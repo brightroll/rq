3a9ba77bdc0b88dff84eeefc6f8fa6f6e62eb109
Kenneth Cheung <kenneth@brightroll.com>
2009-12-28 14:41:21 -0800

    prepend test name to output

--- code/test/run_tests.sh
+++ code/test/run_tests.sh
@@ -3,25 +3,30 @@
 # Send the test queue a test message that should end up in done
 
 
-# Break on any non-zero exit (aka - failure)
-set -e
-
 echo "Must run from 'rq' directory as ./code/test/run_tests.sh"
 
 
 echo "To clean the system, run this command first: ./code/test/setup_test_queues.sh"
 
+for test in \
+   send_test_sneaky.sh \
+   send_test_web_done.rb \
+   send_test_web_prepattachdone.rb \
+   send_test_attachdone.sh \
+   send_test_web_prepdone.rb \
+   send_test_done.sh \
+   send_test_err.sh  \
+   send_test_relay.sh \
+   send_test_remote_relay.sh \
+   send_test_resend.sh  ; do
 
-./code/test/send_test_sneaky.sh
-./code/test/send_test_web_done.rb
-./code/test/send_test_web_prepattachdone.rb
-./code/test/send_test_attachdone.sh
-./code/test/send_test_web_prepdone.rb
-./code/test/send_test_done.sh
-./code/test/send_test_err.sh
-./code/test/send_test_relay.sh
-./code/test/send_test_remote_relay.sh
-./code/test/send_test_resend.sh              
+      output=`./code/test/$test 2>&1`
+      status=$?
+      echo "$output" | sed "s/^/${test}: /g"
+      if [ $status -ne 0 ] ; then
+         exit 1
+      fi
+done
 
 
 echo "-=-=-=-=-=-=-=-=-"
