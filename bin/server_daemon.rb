f39a05268c39be81a14bc70b2547f2981a32a62b
Marshall Beddoe <marshall@brightroll.com>
2010-01-11 15:12:49 -0800

    daemonize the server

---
+++ bin/server_daemon.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/ruby
+
+$:.unshift(File.join(File.dirname(__FILE__), ".."))
+
+Dir.glob(File.join("gems", "gems", "*", "lib")).each do |lib|
+  $LOAD_PATH.unshift(File.expand_path(lib))
+end
+
+Dir.chdir(File.join(File.dirname(__FILE__), ".."))
+
+require 'rubygems'
+gem_paths = [File.expand_path(File.join("gems")),  Gem.default_dir]
+Gem.clear_paths
+Gem.send :set_paths, gem_paths.join(":")
+
+require 'code/router.rb'
+
+builder = MiniRouter.new
+
+pid = fork do
+  Signal.trap('HUP', 'IGNORE') # Don't die upon logout
+  STDIN.reopen("/dev/null")
+  STDOUT.reopen("/dev/null", "w")
+  STDERR.reopen("/dev/null", "w")
+
+  Rack::Handler::WEBrick.run(builder, :Port => 3333)
+end
+
+Process.detach(pid)
+
