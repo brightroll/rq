fc316eacacb49d4e95ccc4b6b692a00b17a53e25
Chris Mague <chris@brightroll.com>
2010-09-08 15:46:56 -0700

    New cleaner script

--- code/cleaner_script.rb
+++ code/cleaner_script.rb
@@ -1,73 +1,145 @@
-#!/bin/bash
+#!/usr/bin/ruby
 
-set -e
+require 'fileutils'
+require 'date'
+require 'time'
+################################################################################
+# Stuff that goes everywhere in RQ
+################################################################################
 
-function write_status {
-  echo $1 $2 >&3
-  echo `/bin/date` $1 $2
-}
+Dir.glob(File.join("..", "..", "..", "..", "..", "code", "vendor", "gems", "*", "lib")).each do |lib|
+  $LOAD_PATH.unshift(File.expand_path(lib))
+end
+Dir.glob(File.join("..", "..", "..", "..", "..")).each do |lib|
+  $LOAD_PATH.unshift(File.expand_path(lib))
+end
 
-write_status 'run'  "starting clean"
+require 'rubygems'
+gem_paths = [File.expand_path(File.join("..", "..", "..", "..", "..", "code", "vendor", "gems")),  Gem.default_dir]
+Gem.clear_paths
+Gem.send :set_paths, gem_paths.join(":")
+# Setup a global binding so the GC doesn't close the file
+$RQ_IO = IO.for_fd(ENV['RQ_PIPE'].to_i)
 
-if [ "X$RQ_PARAM1" = "X" ]; then
-  echo "Empty queue name for cleaner script"
-  write_status 'err' "empty RQ_PARAM1"
-  exit 0
-fi
+# IO tower to RQ mgr process
+def write_status(state, mesg = '')
+  msg = "#{state} #{mesg}\n"
 
-origdir=`pwd`
+  STDOUT.write("#{Process.pid} - #{Time.now} - #{msg}")
+  STDOUT.flush
+  $RQ_IO.syswrite(msg)
+end
 
-if [ "x$RQ_PARAM1" = "xALLQUEUES" ] ; then
-  # special case to clean all queues
-  list=`(cd ../../../.. ; echo *)`
+def handle_fail(mesg = 'soft fail')
+  count = ENV['RQ_COUNT'].to_i
+
+  if count > 15
+    write_status('run', "RQ_COUNT > 15 - failing")
+    write_status('fail', "RQ_COUNT > 15 - failing")
+    exit(0)
+  end
+
+  wait_seconds = count * count * 60
+  write_status('resend', "#{wait_seconds}-#{mesg}")
+  exit(0)
+end
+
+def fail_hard(mesg)
+  write_status('fail', mesg)
+  exit(1)
+end
+
+################################################################################
+# Start the real work here
+################################################################################
+
+def listq(basedir)
+  queues = Dir.glob(basedir + "/queue/??*")
+  queues
+end
+
+def rm_logs_older_than(qname, regex, hours)
+  Dir.glob(qname + "/#{regex}").each do |f|
+    if (Time.now-File.mtime(f))/3600 > hours
+      begin
+        puts "status: removing #{f}"
+        STDOUT.flush
+        FileUtils.rm_rf(f)
+      rescue Exception => e
+        puts e.inspect
+        STDOUT.flush
+        exit(1)
+      end
+    end
+  end
+end
+
+def mv_logs(qname)
+  if File.exists?("#{qname}/queue.log")
+    a=Time.now
+    b = sprintf("%s%.2d%.2d.%.2d:%.2d" ,a.year, a.month, a.day, a.hour, a.min)
+    puts "status: moving #{qname}/queue.log"
+    STDOUT.flush
+    FileUtils.mv("#{qname}/queue.log", "#{qname}/queue.log.#{b}")
+  end
+end
+
+def remove_old(qname, days)
+  clean_queues = ["/done", "/relayed", "/prep", "/queue"]
+  clean_queues.each do |cq|
+    if File.exists?(qname + cq)
+  
+      # go by directories and remove any day dir > days + 1
+      # then go into the hour dirs and remove by time
+      # easier to whack a whole higher level dir then stat everything below it
+      
+      Dir.glob(qname + cq + "/????????").each do |x|
+        if Date.today - Date.strptime(File.basename(x), "%Y%m%d") >= days + 1
+          puts "status: removing " + x
+          STDOUT.flush
+          FileUtils.rm_rf(x)
+        elsif Date.today - Date.strptime(File.basename(x), "%Y%m%d") == days
+          Dir.glob(qname + cq + "/????????/??").each do |y|
+            if y =~ /(\d{8})\/(\d{2})$/
+              timstr = $1 + "."+ $2 + ":00:00"
+              j= DateTime.now - DateTime.strptime(timstr, "%Y%m%d.%H:%M:%S")
+              if j.to_i >= days
+                puts "status: removing " + y
+                STDOUT.flush
+                FileUtils.rm_rf(y)
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  
+end
+  
+
+##################################################################
+# My MAIN
+##################################################################
+basedir = "/rq/current"
+
+if not ENV.has_key?("RQ_PARAM1")
+  fail_hard("need to specify a PARAM1")
+end
+
+if ENV['RQ_PARAM1'] == "ALLQUEUES"
+  queues = listq(basedir)
 else
-  list=${RQ_PARAM1}
-fi
+  queues = [basedir + "/queue/" + ENV['RQ_PARAM1']]
+  if not File.exists?queues[0]
+    fail_hard("the specified queue #{queues} does not exist")
+  end
+end
 
-for q in $list ; do
-  cd $origdir
-  if [ ! -d ../../../../$q ] ; then
-     write_status 'err' "no such queue - $q"
-     exit 0
-  fi
-     
-  cd "../../../../$q" 
-  write_status 'run'  "$q - changed dir `pwd`"
+queues.each do |q|
+  rm_logs_older_than(q, "/queue.log.?*", 2*24)
+  mv_logs(q)
+  remove_old(q, 2)
+end
 
-  write_status 'run'  "$q - rotating log files"
-
-  set +e
-  [ -f queue.log ] && mv queue.log queue.log.`date '+%Y%m%d.%H:%M'`
-  set -e
-  find . -maxdepth 1 -name 'queue.log*' -type f -mtime +2 -printf "old log - %f\n"
-  find . -maxdepth 1 -name 'queue.log*' -type f -mtime +2 -exec /bin/rm -rf {} \;
-
-  write_status 'run'  "$q - cleaning err"
-  cd err
-  find . -maxdepth 1 -type d -mtime +3 -name '??*' | wc -l
-  find . -maxdepth 1 -type d -mtime +3 -name '??*' -exec /bin/rm -rf {} \;
-  cd ..
-
-  write_status 'run'  "$q - cleaning done"
-  cd done
-  find . -maxdepth 2 -d -type d -mtime +2 -name '??*' | wc -l
-  find . -maxdepth 2 -d -type d -mtime +2 -name '??*' -exec /bin/rm -rf {} \;
-  cd ..
-
-  write_status 'run'  "$q - cleaning prep"
-  cd prep
-  find . -maxdepth 1 -type d -mtime +1 -name '??*' | wc -l
-  find . -maxdepth 1 -type d -mtime +1 -name '??*' -exec /bin/rm -rf {} \;
-  cd ..
-
-  write_status 'run'  "$q - cleaning relayed"
-  cd relayed
-  find . -maxdepth 2 -d -type d -mtime +1 -name '??*' | wc -l
-  find . -maxdepth 2 -d -type d -mtime +1 -name '??*' -exec /bin/rm -rf {} \;
-  cd ..
-
-done
-
-echo "FINISHED"
-write_status 'done' "done cleaning"
-
+write_status('done', "successfully ran this script")
