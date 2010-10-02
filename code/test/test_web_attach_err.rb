8c79c023f6b023f1e5361f84287d1a4b75902a66
Dru Nelson <dru@brightroll.com>
2010-10-02 14:41:16 -0700

    TDD on the err attach bug. working/failing test

---
+++ code/test/test_web_attach_err.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+require 'fcntl'
+require 'net/http'
+require 'uri'
+
+require 'test/unit'
+
+class TC_WebAttachErrTest < Test::Unit::TestCase
+  # def setup
+  # end
+
+  # def teardown
+  # end
+  
+  def run_command(cmd)
+    out = `#{cmd}`
+
+    if $?.exitstatus != 0
+      return "err", "cmd failed", ""
+    end
+
+    results = out.split
+
+    [results[0], results, out]
+  end
+
+  def test_num_entries
+    # Create attachement
+    ok,res,_ = run_command("./bin/rq prepmesg  --dest test --src dru4 --relay-ok --param1=err")
+    assert_equal("ok", ok)
+    msg_id = res[1]
+
+    ok,res,out = run_command("./bin/rq attachmesg  --msg_id #{msg_id} --pathname code/test/fixtures/studio3.jpg")
+    assert_equal("ok", ok)
+    expected="ok 14a1a7845cc7f981977fbba6a60f0e42-Attached successfully for Message: #{msg_id} attachment\n"
+    assert_equal(expected, out)
+
+    ok,_,_ = run_command("./bin/rq commitmesg  --msg_id #{msg_id}")
+    assert_equal("ok", ok)
+
+    # Wait for it to change state
+    20.times do
+      |i|
+      ok,res,_ = run_command("./bin/rq statusmesg  --msg_id #{msg_id}")
+      sleep 0.1
+      next if ["prep", "que", "run"].include? res[1]
+      assert_equal("err", res[1])
+    end
+
+    # Check that you can read attachment from err
+    port = ENV["RQ_PORT"] ?  ENV["RQ_PORT"] : "3333"
+    uri_str = "http://127.0.0.1:#{port}/q/test/#{msg_id}/attach/studio3.jpg"
+    res = Net::HTTP.get_response(URI.parse(uri_str))
+    assert_equal(res.code, "200")
+  end
+
+end
+
