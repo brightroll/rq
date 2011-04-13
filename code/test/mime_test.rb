2f5d80202fdac0a0108cc9f5c0804ba431a3e58e
Owen Mehegan <owen@brightroll.com>
2011-04-13 14:08:08 -0700

    When running unit tests on Hudson, specify tmp dir on the same filesystem to avoid errors when attaching files. Also use the RQ_PORT env var if set.

--- code/test/mime_test.rb
+++ code/test/mime_test.rb
@@ -40,7 +40,8 @@ def run(msg_id)
 
   new_hdr = hdr.sub('XXXXMSG_IDXXXX', msg_id)
   data = File.read('code/test/fixtures/bad_mime.data')
-  t = TCPSocket.new('127.0.0.1', 3333)
+  port = ENV['RQ_PORT'] || 3333
+  t = TCPSocket.new('127.0.0.1', port)
 
   write_buff(t, new_hdr)
   write_buff(t, data)
