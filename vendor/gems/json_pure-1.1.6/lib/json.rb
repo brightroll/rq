4468eff1468b070f5ccb1f65229329954b1a2e89
Dru Nelson <dru>
2012-03-20 16:16:04 -0700

    Fix for *finally* isolating vendor json code

--- vendor/gems/json_pure-1.1.6/lib/json.rb
+++ vendor/gems/json_pure-1.1.6/lib/json.rb
@@ -1,10 +1,11 @@
 require 'json/common'
 module JSON
   require 'json/version'
+  require 'json/pure'
 
-  begin
-    require 'json/ext'
-  rescue LoadError
-    require 'json/pure'
-  end
+#  begin
+#    require 'json/ext'
+#  rescue LoadError
+#    require 'json/pure'
+#  end
 end
