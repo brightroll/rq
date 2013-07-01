abdb0e1272424d45171c9a246ffa212188d88a6f
Aaron Stone <aaron@brightroll.com>
2013-07-01 12:46:37 -0500

    Remove unnecessary json include paths from tests

--- test/env_var_test.rb
+++ test/env_var_test.rb
@@ -2,8 +2,6 @@
 require 'test/unit'
 require 'net/http'
 require 'uri'
-
-$LOAD_PATH.unshift(File.expand_path("./vendor/gems/json_pure-1.1.6/lib"))
 require 'json'
 
 class TC_EnvVarTest < Test::Unit::TestCase
