d79274b951556cd9f00a8a201e9779e3812287e6
Aaron Stone <aaron@brightroll.com>
2014-03-29 20:15:51 -0700

    Factor out test code into wait_for_message

--- test/send_dup.rb
+++ test/send_dup.rb
@@ -3,21 +3,11 @@ $: << File.expand_path('..', File.dirname(__FILE__))
 
 require 'vendor/environment'
 require 'test/unit'
-require 'fileutils'
-require 'fcntl'
 require 'net/http'
 require 'uri'
 require 'json'
 
 class TC_SendDupTest < Test::Unit::TestCase
-
-  def setup
-    @rq_port = (ENV['RQ_PORT'] || 3333).to_i
-  end
-
-  # def teardown
-  # end
-
   def run_command(cmd)
     out = `#{cmd}`
 
@@ -30,41 +20,36 @@ class TC_SendDupTest < Test::Unit::TestCase
     [results[0], results, out]
   end
 
-  def test_dup_direct_ok_msg
-    ok,res,_ = run_command("./bin/rq sendmesg  --dest test --src dru4 --relay-ok --param1=dup_direct --param2=/tmp/rq_test_dup_test_rb1")
-    assert_equal("ok", ok)
-    msg_id = res[1]
-
-    # Wait for it to change state
+  # Wait for it to change state
+  def wait_for_message(msg_url)
+    msg_uri = URI.parse(msg_url)
     result = {}
-    20.times do
-      |i|
+    20.times do |i|
       sleep 0.2
-      uri_str = "#{msg_id}.json"
-      res = Net::HTTP.get_response(URI.parse(uri_str))
+      res = Net::HTTP.get_response(msg_uri)
       assert_equal("200", res.code)
       result = JSON.parse(res.body)
       next if ["prep", "que", "run"].include? result['state']
       break
     end
+    result
+  end
+
+  def test_dup_direct_ok_msg
+    ok,res,_ = run_command("./bin/rq sendmesg  --dest test --src dru4 --relay-ok --param1=dup_direct --param2=/tmp/rq_test_dup_test_rb1")
+    assert_equal("ok", ok)
+    msg_id = res[1]
+
+    msg_url = "#{msg_id}.json"
+    result = wait_for_message(msg_url)
 
     assert_equal("done", result['state'])
 
     new_msg_id = File.read("/tmp/rq_test_dup_test_rb1").strip
     File.unlink("/tmp/rq_test_dup_test_rb1")
 
-    # Wait for it to change state
-    result = {}
-    20.times do
-      |i|
-      sleep 0.2
-      uri_str = "#{new_msg_id}.json"
-      res = Net::HTTP.get_response(URI.parse(uri_str))
-      assert_equal("200", res.code)
-      result = JSON.parse(res.body)
-      next if ["prep", "que", "run"].include? result['state']
-      break
-    end
+    msg_url = "#{new_msg_id}.json"
+    result = wait_for_message(msg_url)
 
     assert_equal("done", result['state'])
     assert_equal("dup_direct", result['param1'])
@@ -76,36 +61,16 @@ class TC_SendDupTest < Test::Unit::TestCase
     assert_equal("ok", ok)
     msg_id = res[1]
 
-    # Wait for it to change state (relayed)
-    result = {}
-    20.times do
-      |i|
-      sleep 0.2
-      uri_str = "#{msg_id}.json"
-      res = Net::HTTP.get_response(URI.parse(uri_str))
-      assert_equal("200", res.code)
-      result = JSON.parse(res.body)
-      next if ["prep", "que", "run"].include? result['state']
-      break
-    end
+    msg_url = "#{msg_id}.json"
+    result = wait_for_message(msg_url)
 
     assert_equal("done", result['state'])
 
     new_msg_id = File.read("/tmp/rq_test_dup_test_rb3").strip
     File.unlink("/tmp/rq_test_dup_test_rb3")
 
-    # Wait for it to change state
-    result = {}
-    20.times do
-      |i|
-      sleep 0.2
-      uri_str = "#{new_msg_id}.json"
-      res = Net::HTTP.get_response(URI.parse(uri_str))
-      assert_equal("200", res.code)
-      result = JSON.parse(res.body)
-      next if ["prep", "que", "run"].include? result['state']
-      break
-    end
+    msg_url = "#{new_msg_id}.json"
+    result = wait_for_message(msg_url)
 
     assert_equal("relayed", result['state'])
     assert_equal("dup_relay", result['param1'])
@@ -113,18 +78,8 @@ class TC_SendDupTest < Test::Unit::TestCase
 
     next_msg_id = result['status'].split(' - ', 2)[1]
 
-    # Wait for it to go to done
-    result = {}
-    20.times do
-      |i|
-      sleep 0.2
-      uri_str = "#{next_msg_id}.json"
-      res = Net::HTTP.get_response(URI.parse(uri_str))
-      assert_equal("200", res.code)
-      result = JSON.parse(res.body)
-      next if ["prep", "que", "run"].include? result['state']
-      break
-    end
+    msg_url = "#{next_msg_id}.json"
+    result = wait_for_message(msg_url)
 
     assert_equal("done", result['state'])
     assert_equal("dup_relay", result['param1'])
@@ -144,36 +99,16 @@ class TC_SendDupTest < Test::Unit::TestCase
     ok,_,_ = run_command("./bin/rq commitmesg  --msg_id #{msg_id}")
     assert_equal("ok", ok)
 
-    # Wait for it to change state
-    result = {}
-    20.times do
-      |i|
-      sleep 0.2
-      uri_str = "#{msg_id}.json"
-      res = Net::HTTP.get_response(URI.parse(uri_str))
-      assert_equal("200", res.code)
-      result = JSON.parse(res.body)
-      next if ["prep", "que", "run"].include? result['state']
-      break
-    end
+    msg_url = "#{msg_id}.json"
+    result = wait_for_message(msg_url)
 
     assert_equal("done", result['state'])
 
     new_msg_id = File.read("/tmp/rq_test_dup_test_rb5").strip
     File.unlink("/tmp/rq_test_dup_test_rb5")
 
-    # Wait for it to change state
-    result = {}
-    20.times do
-      |i|
-      sleep 0.2
-      uri_str = "#{new_msg_id}.json"
-      res = Net::HTTP.get_response(URI.parse(uri_str))
-      assert_equal("200", res.code)
-      result = JSON.parse(res.body)
-      next if ["prep", "que", "run"].include? result['state']
-      break
-    end
+    msg_url = "#{new_msg_id}.json"
+    result = wait_for_message(msg_url)
 
     assert_equal("done", result['state'])
     assert_equal("dup_direct", result['param1'])
@@ -198,36 +133,16 @@ class TC_SendDupTest < Test::Unit::TestCase
     ok,_,_ = run_command("./bin/rq commitmesg  --msg_id #{msg_id}")
     assert_equal("ok", ok)
 
-    # Wait for it to change state
-    result = {}
-    20.times do
-      |i|
-      sleep 0.2
-      uri_str = "#{msg_id}.json"
-      res = Net::HTTP.get_response(URI.parse(uri_str))
-      assert_equal("200", res.code)
-      result = JSON.parse(res.body)
-      next if ["prep", "que", "run"].include? result['state']
-      break
-    end
+    msg_url = "#{msg_id}.json"
+    result = wait_for_message(msg_url)
 
     assert_equal("done", result['state'])
 
     new_msg_id = File.read("/tmp/rq_test_dup_test_rb6").strip
     File.unlink("/tmp/rq_test_dup_test_rb6")
 
-    # Wait for it to change state (relay)
-    result = {}
-    20.times do
-      |i|
-      sleep 0.2
-      uri_str = "#{new_msg_id}.json"
-      res = Net::HTTP.get_response(URI.parse(uri_str))
-      assert_equal("200", res.code)
-      result = JSON.parse(res.body)
-      next if ["prep", "que", "run"].include? result['state']
-      break
-    end
+    msg_url = "#{new_msg_id}.json"
+    result = wait_for_message(msg_url)
 
     assert_equal("relayed", result['state'])
     assert_equal("dup_relay", result['param1'])
@@ -235,18 +150,8 @@ class TC_SendDupTest < Test::Unit::TestCase
 
     next_msg_id = result['status'].split(' - ', 2)[1]
 
-    # Wait for it to go to done
-    result = {}
-    20.times do
-      |i|
-      sleep 0.2
-      uri_str = "#{next_msg_id}.json"
-      res = Net::HTTP.get_response(URI.parse(uri_str))
-      assert_equal("200", res.code)
-      result = JSON.parse(res.body)
-      next if ["prep", "que", "run"].include? result['state']
-      break
-    end
+    msg_url = "#{next_msg_id}.json"
+    result = wait_for_message(msg_url)
 
     assert_equal("done", result['state'])
     assert_equal("dup_relay", result['param1'])
@@ -263,18 +168,8 @@ class TC_SendDupTest < Test::Unit::TestCase
     assert_equal("ok", ok)
     msg_id = res[1]
 
-    # Wait for it to change state
-    result = {}
-    20.times do
-      |i|
-      sleep 0.2
-      uri_str = "#{msg_id}.json"
-      res = Net::HTTP.get_response(URI.parse(uri_str))
-      assert_equal("200", res.code)
-      result = JSON.parse(res.body)
-      next if ["prep", "que", "run"].include? result['state']
-      break
-    end
+    msg_url = "#{msg_id}.json"
+    result = wait_for_message(msg_url)
 
     assert_equal("done", result['state'])
 
@@ -283,6 +178,4 @@ class TC_SendDupTest < Test::Unit::TestCase
 
     assert_equal("fail couldn't connect to queue - nope_this_q_does_not_exist", response)
   end
-
 end
-
