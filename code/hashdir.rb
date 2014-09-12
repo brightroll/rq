8eba97421966c2ffb289f92081f756e79fe2b100
Aaron Stone <aaron@brightroll.com>
2014-09-11 22:54:31 -0700

    Mark internal methods private

--- code/hashdir.rb
+++ code/hashdir.rb
@@ -24,6 +24,52 @@ module RQ
       self.entries_int(path, 0, [], limit)
     end
 
+    def self.num_entries(path)
+      self.num_entries_int(path, 0)
+    end
+
+    def self.inject(prev_msg_path, new_base_path, msg_id)
+      parts = self.msg_id_parts(msg_id)
+      FileUtils.mkdir_p("#{new_base_path}/#{parts[0]}/#{parts[1]}/#{parts[2]}")
+      newname = "#{new_base_path}/#{parts[0]}/#{parts[1]}/#{parts[2]}/#{msg_id}"
+      File.rename(prev_msg_path, newname)
+    end
+
+    def self.path_for(que_base_path, msg_id)
+      parts = self.msg_id_parts(msg_id)
+      "#{que_base_path}/#{parts[0]}/#{parts[1]}/#{parts[2]}/#{msg_id}"
+    end
+
+    private
+
+    def self.num_entries_int(path, level)
+      sum = 0
+      if level == 0
+        # YYYYMMDD
+        ents1 = Dir.glob("#{path}/[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]")
+        ents1.sort.reverse.each do |e|
+          sum += self.num_entries_int(e, 1)
+        end
+      elsif level == 1
+        # HH
+        ents1 = Dir.glob("#{path}/[0-9][0-9]")
+        ents1.sort.reverse.each do |e|
+          sum += self.num_entries_int(e, 2)
+        end
+      elsif level == 2
+        # MM
+        ents1 = Dir.glob("#{path}/[0-9][0-9]")
+        ents1.sort.reverse.each do |e|
+          sum += self.num_entries_int(e, 3)
+        end
+      elsif level == 3
+        # MESG-ID
+        ents1 = Dir.glob("#{path}/[0-9][0-9]*")
+        sum += ents1.length
+      end
+      sum
+    end
+
     def self.entries_int(path, level, accum, limit = nil)
       if level == 0
         # YYYYMMDD
@@ -55,64 +101,6 @@ module RQ
         end
       end
       accum
-    end
-
-    def self.entries_old(path, limit = nil)
-      # Do a depth first walk of the path to get the message ids
-      ents1 = Dir.glob("#{path}/**/msg")
-      # we want the msg_id part (-2), just before last (-1)
-      ents2 = ents1.map { |e| e.split('/')[-2] }
-    end
-
-    def self.num_entries(path)
-      self.num_entries_int(path, 0)
-    end
-
-    def self.num_entries_int(path, level)
-      sum = 0
-      if level == 0
-        # YYYYMMDD
-        ents1 = Dir.glob("#{path}/[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]")
-        ents1.sort.reverse.each do |e|
-          sum += self.num_entries_int(e, 1)
-        end
-      elsif level == 1
-        # HH
-        ents1 = Dir.glob("#{path}/[0-9][0-9]")
-        ents1.sort.reverse.each do |e|
-          sum += self.num_entries_int(e, 2)
-        end
-      elsif level == 2
-        # MM
-        ents1 = Dir.glob("#{path}/[0-9][0-9]")
-        ents1.sort.reverse.each do |e|
-          sum += self.num_entries_int(e, 3)
-        end
-      elsif level == 3
-        # MESG-ID
-        ents1 = Dir.glob("#{path}/[0-9][0-9]*")
-        sum += ents1.length
-      end
-      sum
-    end
-
-    def self.num_entries_old(path)
-      # Do a depth first walk of the path to get the message ids
-      ents1 = Dir.glob("#{path}/**/msg")
-      ents1.length
-    end
-
-
-    def self.inject(prev_msg_path, new_base_path, msg_id)
-      parts = self.msg_id_parts(msg_id)
-      FileUtils.mkdir_p("#{new_base_path}/#{parts[0]}/#{parts[1]}/#{parts[2]}")
-      newname = "#{new_base_path}/#{parts[0]}/#{parts[1]}/#{parts[2]}/#{msg_id}"
-      File.rename(prev_msg_path, newname)
-    end
-
-    def self.path_for(que_base_path, msg_id)
-      parts = self.msg_id_parts(msg_id)
-      "#{que_base_path}/#{parts[0]}/#{parts[1]}/#{parts[2]}/#{msg_id}"
     end
 
     def self.msg_id_parts(msg_id)
