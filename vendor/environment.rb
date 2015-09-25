31b46ade4dd164887ecb90a49d7f4cee6180b4c2
Aaron Stone <aaron@brightroll.com>
2015-09-25 14:22:00 -0700

    Update rack and sinatra gems

--- vendor/environment.rb
+++ vendor/environment.rb
@@ -9,10 +9,10 @@ def rqrq_rqenv_add_path_if_needed(path)
 end
 
 rqrq_rqenv_add_path_if_needed(File.expand_path("#{dir}/gems/json_pure-1.8.1/lib"))
-rqrq_rqenv_add_path_if_needed(File.expand_path("#{dir}/gems/rack-1.5.2/lib"))
+rqrq_rqenv_add_path_if_needed(File.expand_path("#{dir}/gems/rack-1.5.5/lib"))
 rqrq_rqenv_add_path_if_needed(File.expand_path("#{dir}/gems/tilt-1.4.1/lib"))
 rqrq_rqenv_add_path_if_needed(File.expand_path("#{dir}/gems/rack-protection-1.5.3/lib"))
-rqrq_rqenv_add_path_if_needed(File.expand_path("#{dir}/gems/sinatra-1.4.5/lib"))
+rqrq_rqenv_add_path_if_needed(File.expand_path("#{dir}/gems/sinatra-1.4.6/lib"))
 rqrq_rqenv_add_path_if_needed(File.expand_path("#{dir}/gems/unixrack-1.0.4.1/lib"))
 rqrq_rqenv_add_path_if_needed(File.expand_path("#{dir}/gems/daemons-1.2.3/lib"))
 rqrq_rqenv_add_path_if_needed(File.expand_path("#{dir}/gems/parse-cron-0.1.4/lib"))
